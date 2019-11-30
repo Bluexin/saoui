2ff8a97a2f64cffe7bfe6eeeb9d02635ee83c32e
Tencao <chris9282@Hotmail.co.uk>
2019-11-30 19:15:11 +0000

    Bump Version

--- src/main/java/com/saomc/saoui/SAOCore.kt
+++ src/main/java/com/saomc/saoui/SAOCore.kt
@@ -45,7 +45,7 @@ import javax.xml.bind.JAXBException
 object SAOCore {
     const val MODID = "saoui"
     const val NAME = "Sword Art Online UI"
-    const val VERSION = "2.0.7"
+    const val VERSION = "2.0.8"
     const val DEPS = "required-after:saomclib@[1.4.1,);after:mantle;after:librarianlib@[4.19.2,)"
 
     @JvmStatic
